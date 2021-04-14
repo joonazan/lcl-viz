5a8846122623953d5faf66e2ae2b6ee6c09c5a4f
Joonatan Saarhelo <joonatan.saarhelo@aalto.fi>
2021-04-14 23:58:49 +0300

    produce a random walk of three-colorings

---
+++ src/grid.rs
@@ -0,0 +1,40 @@
+use std::ops::{Index, IndexMut};
+
+pub struct Grid<T, const N: usize>(pub [[T; N]; N]);
+
+type Coord = (usize, usize);
+
+impl<T, const N: usize> Grid<T, N>
+where
+    T: Default + Copy,
+{
+    pub fn new() -> Self {
+        Self([[T::default(); N]; N])
+    }
+}
+
+impl<T, const N: usize> Grid<T, N> {
+    pub fn neighbors((x, y): Coord) -> impl Iterator<Item = Coord> {
+        let one_axis = |x| {
+            if x > 0 { Some(x - 1) } else { None }
+                .into_iter()
+                .chain(if x + 1 < N { Some(x + 1) } else { None }.into_iter())
+        };
+        one_axis(x)
+            .map(move |x| (x, y))
+            .chain(one_axis(y).map(move |y| (x, y)))
+    }
+}
+
+impl<T, const N: usize> Index<Coord> for Grid<T, N> {
+    type Output = T;
+    fn index(&self, (x, y): Coord) -> &Self::Output {
+        &self.0[x][y]
+    }
+}
+
+impl<T, const N: usize> IndexMut<Coord> for Grid<T, N> {
+    fn index_mut(&mut self, (x, y): Coord) -> &mut Self::Output {
+        &mut self.0[x][y]
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 use rand::prelude::*;
+use rand::seq::IteratorRandom;
 use std::cell::RefCell;
 use std::convert::TryInto;
 use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::Element;
+mod grid;
+use grid::Grid;
 
 #[wasm_bindgen]
 extern "C" {
@@ -17,21 +20,23 @@ pub fn start() -> Result<(), JsValue> {
     let document = window.document().unwrap();
 
     const N: usize = 25;
-    let nodes: [[Element; N]; N] = (0..N)
-        .map(|_| {
-            (0..N)
-                .map(|_| document.create_element("div").unwrap())
-                .collect::<Vec<_>>()
-                .try_into()
-                .unwrap()
-        })
-        .collect::<Vec<_>>()
-        .try_into()
-        .unwrap();
+    let nodes: Grid<Element, N> = Grid(
+        (0..N)
+            .map(|_| {
+                (0..N)
+                    .map(|_| document.create_element("div").unwrap())
+                    .collect::<Vec<_>>()
+                    .try_into()
+                    .unwrap()
+            })
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap(),
+    );
 
     let container = document.create_element("div")?;
     container.set_class_name("container");
-    for row_nodes in &nodes {
+    for row_nodes in &nodes.0 {
         let row = document.create_element("div")?;
         row.set_class_name("row");
         container.append_child(&row)?;
@@ -47,16 +52,46 @@ pub fn start() -> Result<(), JsValue> {
     let closure = Rc::new(RefCell::new(None));
     let closure2 = closure.clone();
 
+    let mut g = Grid::<u8, N>::new();
+
     for y in 0..N {
         for x in 0..N {
             let color = (x ^ y) & 1;
-            nodes[y][x].set_class_name(color_names[color]);
+            g[(x, y)] = color as u8;
+            nodes[(x, y)].set_class_name(color_names[color]);
         }
     }
 
     *closure2.borrow_mut() = Some(Closure::wrap(Box::new(move || {
-        nodes[rng.gen_range(0..N)][rng.gen_range(0..N)]
-            .set_class_name(color_names[rng.gen_range(0..3)]);
+        let mut p;
+        let mut p2;
+        let mut c;
+        let mut c2;
+        loop {
+            p = (rng.gen_range(0..N), rng.gen_range(0..N));
+            p2 = Grid::<u8, N>::neighbors(p).choose(&mut rng).unwrap();
+
+            c = rng.gen_range(0..3);
+            c2 = rng.gen_range(0..3);
+
+            let old = g[p];
+            let old2 = g[p2];
+
+            g[p] = c;
+            g[p2] = c2;
+
+            if Grid::<u8, N>::neighbors(p).all(|n| g[n] != g[p])
+                && Grid::<u8, N>::neighbors(p2).all(|n| g[n] != g[p2])
+            {
+                break;
+            }
+
+            g[p] = old;
+            g[p2] = old2;
+        }
+
+        nodes[p].set_class_name(color_names[c as usize]);
+        nodes[p2].set_class_name(color_names[c2 as usize]);
 
         requestAnimationFrame(closure.borrow().as_ref().unwrap());
     }) as Box<dyn FnMut()>));
