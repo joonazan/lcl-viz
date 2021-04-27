4aff90916f90f0c141201ef60fff0b3272f44ade
Joonatan Saarhelo <joon.saar@gmail.com>
2021-04-27 16:35:08 +0300

    use yew instead of raw DOM bindings

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,7 @@
-use rand::prelude::*;
-use std::cell::RefCell;
-use std::rc::Rc;
-use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
-mod fivecolor;
-mod grid;
+#![recursion_limit = "1024"]
+#![feature(iter_intersperse)]
 mod necklace;
-use fivecolor::*;
-
-#[wasm_bindgen]
-extern "C" {
-    fn requestAnimationFrame(closure: &Closure<dyn FnMut()>) -> u32;
-}
 
 pub fn main() {
-    let document = web_sys::window().unwrap().document().unwrap();
-
-    let mut rng = rand::rngs::StdRng::seed_from_u64(0);
-    //let mut fc = FiveColor::new(&mut rng, &document);
-    document
-        .body()
-        .unwrap()
-        .append_child(&necklace::create(&document)).unwrap();
-
-    let closure = Rc::new(RefCell::new(None));
-    let closure2 = closure.clone();
-    *closure2.borrow_mut() = Some(Closure::wrap(Box::new(move || {
-        //fc.evolve(&mut rng);
-        requestAnimationFrame(closure.borrow().as_ref().unwrap());
-    }) as Box<dyn FnMut()>));
-    requestAnimationFrame(closure2.borrow().as_ref().unwrap());
+    yew::start_app::<necklace::Necklace>();
 }
--- src/necklace.rs
+++ src/necklace.rs
@@ -1,143 +1,222 @@
-use std::rc::Rc;
-use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
-use web_sys::{Document, Element, HtmlElement, HtmlInputElement};
+use web_sys::HtmlElement;
+use yew::prelude::*;
 
-pub fn create(document: &Document) -> Rc<HtmlElement> {
-    let container = document
-        .create_element("div")
-        .unwrap()
-        .dyn_into::<HtmlElement>()
-        .unwrap();
-    container.set_id("necklace");
-    let container = Rc::new(container);
-
-    container
-        .append_child(&make_text(document, "h2", "Pick two colors"))
-        .unwrap();
-
-    container
-        .append_child(&color_picker(
-            document,
-            container.clone(),
-            "--color1",
-            "#9be54d",
-        ))
-        .unwrap();
-    container
-        .append_child(&color_picker(
-            document,
-            container.clone(),
-            "--color2",
-            "#383cc0",
-        ))
-        .unwrap();
-
-    container
-        .append_child(&make_text(document, "h2", "Proper coloring"))
-        .unwrap();
-    container
-        .append_child(&make_text(document, "p",
-                                 "Your job here is to color beads so that the chain doesn't have two identical colors next to each other. Below is an example of a proper coloring."))
-        .unwrap();
-
-    let (path, vertices) = make_necklace(document, 7, false, false);
-    for (i, b) in vertices.iter().enumerate() {
-        b.set_attribute("data-color", if i & 1 == 0 { "A" } else { "B" })
-            .unwrap();
-    }
-    container.append_child(&path).unwrap();
-
-    container.append_child(&make_text(document, "p", "But you only get to see a neighborhood of a certain size. And based on that, you have to choose a color.")).unwrap();
-
-    let (path2, vertices2) = make_necklace(document, 3, true, true);
-    for (i, b) in vertices2.iter().enumerate() {
-        b.set_inner_html(&format!("{}", i));
-    }
-    container.append_child(&path2).unwrap();
-
-    container
+pub struct Necklace {
+    link: ComponentLink<Self>,
+    container: NodeRef,
 }
 
-fn make_necklace(
-    document: &Document,
-    n: usize,
-    open_start: bool,
-    open_end: bool,
-) -> (Element, Vec<Element>) {
-    let chain = document.create_element("div").unwrap();
-    chain.set_class_name("chain");
-
-    let edge = || {
-        let edge = document.create_element("div").unwrap();
-        edge.set_class_name("edge");
-        chain.append_child(&edge).unwrap();
-    };
-
-    let bead = || {
-        let bead = document.create_element("div").unwrap();
-        bead.set_class_name("bead");
-        chain.append_child(&bead).unwrap();
-        bead
-    };
-
-    let beads = if !open_start {
-        Some(bead())
-    } else {
-        None
-    };
-
-    let beads = beads.into_iter().chain((0..n - if open_start { 0 } else { 1 })
-        .map(|_| {
-            edge();
-            bead()
-        }))
-        .collect::<Vec<_>>();
-
-    if open_end {
-        edge();
-    }
-
-    (chain, beads)
-}
-
-fn make_text(document: &Document, tag: &'static str, text: &'static str) -> Element {
-    let h = document.create_element(tag).unwrap();
-    h.set_inner_html(text);
-    h
-}
-
-fn color_picker(
-    document: &Document,
-    container: Rc<HtmlElement>,
-    color_name: &'static str,
-    init_color: &'static str,
-) -> HtmlInputElement {
-    let picker = document.create_element("input").unwrap();
-    let picker = picker.dyn_into::<HtmlInputElement>().unwrap();
-    picker.set_type("color");
-    picker.set_value(init_color);
-    container
-        .style()
-        .set_property(color_name, init_color)
-        .unwrap();
-
-    let closure = Closure::wrap(Box::new(move |event: web_sys::InputEvent| {
-        container
+impl Necklace {
+    fn set_css_var(&mut self, name: &str, value: &str) {
+        self.container
+            .cast::<HtmlElement>()
+            .unwrap()
             .style()
-            .set_property(
-                color_name,
-                &event
-                    .target()
-                    .unwrap()
-                    .dyn_into::<HtmlInputElement>()
-                    .unwrap()
-                    .value(),
-            )
+            .set_property(name, value)
             .unwrap();
-    }) as Box<dyn FnMut(_)>);
-    picker.set_oninput(Some(closure.as_ref().unchecked_ref()));
-    closure.forget();
+    }
+}
 
-    picker
+const COLOR1: &str = "#9be54d";
+const COLOR2: &str = "#383cc0";
+
+pub enum Msg {
+    ChangeColor(&'static str, String),
+}
+use Msg::*;
+
+impl Component for Necklace {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
+            container: NodeRef::default(),
+        }
+    }
+
+    fn rendered(&mut self, first_render: bool) {
+        if first_render {
+            self.set_css_var("--color1", COLOR1);
+            self.set_css_var("--color2", COLOR2);
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            ChangeColor(color, value) => self.set_css_var(color, &value),
+        }
+        false
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div id="necklace" ref=self.container.clone()>
+                <h2>{"Pick two colors"}</h2>
+                <input type="color" value=COLOR1 oninput=self.link.callback(|input: InputData| ChangeColor("--color1", input.value))/>
+                <input type="color" value=COLOR2 oninput=self.link.callback(|input: InputData| ChangeColor("--color2", input.value))/>
+                <h2>{"Proper coloring"}</h2>
+                <p>{"Your job here is to color beads so that the chain doesn't have two identical colors next to each other. Below is an example of a proper coloring."}</p>
+                <Chain colors=vec![Some(false), Some(true), Some(false), Some(true), Some(false)] />
+                <NoIds/>
+            </div>
+        }
+    }
+}
+
+struct NoIds {
+    color: Option<bool>,
+    color2: Option<bool>,
+    link: ComponentLink<Self>,
+}
+enum NoIdsMsg {
+    FirstPick(bool),
+    SecondPick(bool),
+    Reset,
+}
+use NoIdsMsg::*;
+
+impl Component for NoIds {
+    type Message = NoIdsMsg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
+            color: None,
+            color2: None,
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            FirstPick(c) => self.color = Some(c),
+            SecondPick(c) => self.color2 = Some(c),
+            Reset => self.color2 = None,
+        }
+        true
+    }
+
+    fn change(&mut self, _: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        let chain = html! {<Chain colors=vec![self.color, None, None, None] highlight=Some(0) />};
+        html! {
+            <>
+                <h2>{"Independence"}</h2>
+                <p>{"Each bead has to independently choose its color using the same reasoning. Let's look at a length four chain as an example."}</p>
+            {chain}
+            {if let Some(c) = self.color {
+                let chain2 = html!{<Chain colors=vec![None, None, None, self.color2] highlight=Some(3) />};
+                if let Some(c2) = self.color2 {
+                    html! { <>
+                    {if c != c2 {
+                        html!{ <>
+                           <p>{"But this contradicts with the first choice!."}</p>
+                            <div class="rotate">{chain2}</div>
+                            </> }
+                    } else {
+                        html!{ <>
+                             <p>{"But now it is impossible to color the beads in between!"}</p>
+                              {chain2}
+                        </> }
+                    }}
+                     <button onclick=self.link.callback(|_| Reset)>{"Try again"}</button>
+                    </> }
+
+                } else {
+                    html!{ <>
+                        <p>{"What about this one?"}</p>
+                         {chain2}
+                        {colorpick(self.link.callback(SecondPick))}
+                        </> }
+                }
+            } else {
+                html!{ <>
+                     <p>{"What color should the above bead have?"}</p>
+                      {colorpick(self.link.callback(FirstPick))}
+                      </> }
+            }}
+            </>
+        }
+    }
+}
+
+fn colorpick(cb: Callback<bool>) -> Html {
+    html! { <>
+        <button class="colorbutton" data-color="A" onclick=cb.reform(|_|false)/>{" or "}<button class="colorbutton" data-color="B" onclick=cb.reform(|_|true)/>{"?"}
+    </> }
+}
+
+#[derive(Clone, Properties, PartialEq)]
+struct Chain {
+    colors: Vec<Option<bool>>,
+    #[prop_or_default]
+    numbers: Option<Vec<usize>>,
+    #[prop_or_default]
+    open_start: bool,
+    #[prop_or_default]
+    open_end: bool,
+    #[prop_or_default]
+    highlight: Option<usize>,
+}
+
+impl Component for Chain {
+    type Message = ();
+    type Properties = Chain;
+
+    fn create(props: Self::Properties, _: ComponentLink<Self>) -> Self {
+        props
+    }
+
+    fn update(&mut self, _: Self::Message) -> ShouldRender {
+        false
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        *self = props;
+        true
+    }
+
+    fn view(&self) -> Html {
+        let beads = self.colors.iter().enumerate().map(|(i, color)| {
+            let class = if Some(i) == self.highlight {
+                "bead highlight"
+            } else {
+                "bead"
+            };
+            if let Some(c) = color {
+                html! {<div class=class data-color=if *c {"B"} else {"A"}></div>}
+            } else {
+                html! {<div class=class></div>}
+            }
+        });
+
+        let edge = html! {<div class="edge"></div>};
+        let chain = if self.open_start {
+            Some(edge.clone())
+        } else {
+            None
+        }
+        .into_iter()
+        .chain(beads.intersperse(edge.clone()))
+        .chain(if self.open_end {
+            Some(edge.clone())
+        } else {
+            None
+        });
+
+        html! {
+            <div class="chain">
+                {for chain}
+            </div>
+        }
+    }
 }
