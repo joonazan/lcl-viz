eee869844e699c92e9f6c96f5bf044fcc5ee6318
Joonatan Saarhelo <joonatan.saarhelo@aalto.fi>
2021-04-28 17:23:24 +0300

    finalize the section about not having ids

--- src/necklace/no_ids.rs
+++ src/necklace/no_ids.rs
@@ -2,14 +2,12 @@ use super::utils::*;
 use yew::prelude::*;
 
 pub struct NoIds {
-    color: Option<bool>,
-    color2: Option<bool>,
+    colors: Vec<Option<bool>>,
     link: ComponentLink<Self>,
     finished: Callback<()>,
 }
 pub enum Msg {
-    FirstPick(bool),
-    SecondPick(bool),
+    PickColor(usize, bool),
     Reset,
 }
 use Msg::*;
@@ -21,17 +19,15 @@ impl Component for NoIds {
     fn create(p: Self::Properties, link: ComponentLink<Self>) -> Self {
         Self {
             link,
-            color: None,
-            color2: None,
+            colors: vec![None; 4],
             finished: p.finished,
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            FirstPick(c) => self.color = Some(c),
-            SecondPick(c) => self.color2 = Some(c),
-            Reset => self.color2 = None,
+            PickColor(i, c) => self.colors[i] = Some(c),
+            Reset => self.colors = vec![None; 4],
         }
         true
     }
@@ -42,45 +38,77 @@ impl Component for NoIds {
     }
 
     fn view(&self) -> Html {
-        let chain = html! {<Chain colors=vec![self.color, None, None, None] highlight=Some(0) />};
-        html! {
-            <>
-                <h2>{"Independence"}</h2>
-                <p>{"Each bead has to independently choose its color using the same reasoning. Let's look at a length four chain as an example."}</p>
-            {chain}
-            {if let Some(c) = self.color {
-                let chain2 = html!{<Chain colors=vec![None, None, None, self.color2] highlight=Some(3) />};
-                if let Some(c2) = self.color2 {
-                    html! { <>
-                    {if c != c2 {
-                        html!{ <>
-                           <p>{"But this contradicts with the first choice!."}</p>
-                            <div class="rotate">{chain2}</div>
-                            </> }
-                    } else {
-                        html!{ <>
-                             <p>{"But now it is impossible to color the beads in between!"}</p>
-                              {chain2}
-                        </> }
-                    }}
-                     <button onclick=self.link.callback(|_| Reset)>{"Try again"}</button>
-                     <button onclick=self.finished.reform(|_|())>{"This is impossible"}</button>
-                    </> }
-
-                } else {
-                    html!{ <>
-                        <p>{"What about this one?"}</p>
-                         {chain2}
-                        {colorpick(self.link.callback(SecondPick))}
-                        </> }
-                }
-            } else {
-                html!{ <>
-                     <p>{"What color should the above bead have?"}</p>
-                      {colorpick(self.link.callback(FirstPick))}
-                      </> }
-            }}
-            </>
+        fn differ(a: Option<bool>, b: Option<bool>) -> bool {
+            a.and_then(|a| b.map(|b| a != b)).unwrap_or(false)
         }
+        let contradiction1 = differ(self.colors[0], self.colors[3]);
+        let contradiction2 = differ(self.colors[1], self.colors[2]);
+        let improper_coloring = self
+            .colors
+            .iter()
+            .zip(self.colors.iter().skip(1))
+            .any(|(&a, &b)| a.and_then(|a| b.map(|b| a == b)).unwrap_or(false));
+        let error = contradiction1 || contradiction2 || improper_coloring;
+
+        let choice_chain = |i: usize| {
+            let cols = self
+                .colors
+                .iter()
+                .enumerate()
+                .map(|(j, c)| if i == j { *c } else { None })
+                .collect::<Vec<_>>();
+            let not_chosen = self.colors[i] == None;
+            let message = [
+                "What color should the below bead have?",
+                "Now, what should this one's color be?",
+                "One left!",
+                "What about this one?",
+            ][i];
+            let chain = html! {<Chain colors=cols highlight=i />};
+
+            html! {<>
+                  {if not_chosen {html!{<p>{message}</p>}} else {html!{}}}
+                  {if contradiction1 && i == 3 || contradiction2 && i == 2 {
+                      html!{<>
+                            <p>{"But this contradicts the previous choice!"}</p>
+                            <div class="rotate">{chain}</div>
+                      </>}
+                  } else {
+                      chain
+                  }}
+                  {if not_chosen {colorpick(self.link.callback(move |c| PickColor(i, c)))} else {html!{}}}
+            </> }
+        };
+        let nof_colored = self.colors.iter().filter(|&&x| x != None).count();
+        let order = [0, 3, 1, 2];
+
+        html! { <section>
+            <h2>{"Independence"}</h2>
+            <p>{"Each bead has to independently choose its color using the same reasoning. Let's look at a length four chain as an example."}</p>
+            <Chain colors=self.colors.clone() />
+
+            {if nof_colored != 0 {
+                html!{<>
+                    <h3>{"Past choices"}</h3>
+                    {for order.iter().take(nof_colored).cloned().map(choice_chain)}
+                </>}
+            } else {
+                html!{}
+            }}
+
+            {if improper_coloring {
+                html!{<p>{"But now two beads with the same color are next to each other!"}</p>}
+            } else {
+                html!{}
+            }}
+            {if error {
+                html!{ <>
+                    <button onclick=self.link.callback(|_| Reset)>{"Try again"}</button>
+                    <button onclick=self.finished.reform(|_|())>{"This is impossible"}</button>
+                </> }
+            } else {
+                {choice_chain(order[nof_colored])}
+            }}
+        </section> }
     }
 }
